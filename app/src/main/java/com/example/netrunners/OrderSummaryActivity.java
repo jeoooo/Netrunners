09463f141b3a1426cb67eae07543098c5ca3fa99
RedenLamosa <RedenLamosa@users.noreply.github.com>
2020-12-21 20:43:18 +0800

    OrderSummaryActivity added

---
+++ app/src/main/java/com/example/netrunners/OrderSummaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.netrunners;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_summary);
+    }
+}
